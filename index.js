715ca3e6e9d592b4d8de9870a4b7537e4ea32535
Sagar Behara <sagar@cryptocontrol.io>
2021-06-26 00:14:05 +0530

    added cache

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 import { getArthWethLPTokenPrice, getArthMahaLPTokenPrice, getArthxWethLPTokenPrice } from './controller/APY';
 import routes from './routes'
+import apicache from 'apicache'
 
 const express = require('express')
+let cache = apicache.middleware
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -10,6 +12,7 @@ app.use(cors())
 app.use(bodyParser.json({ limit: '100mb' }))
 app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }))
 app.use(routes)
+app.use(cache('3 minutes'))
 
 app.get('/', function (req, res) {
     res.send('Hello World')
