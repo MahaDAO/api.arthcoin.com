af78fc6aee083acfcd839f4f5ca5e775429afd61
Sagar Behara <sagar@cryptocontrol.io>
2021-07-02 13:20:57 +0530

    copied the remaing pools

--- controller/APY.js
+++ controller/APY.js
@@ -1,16 +1,8 @@
-//imp
 const Web3 = require('web3');
-const Provider = require('@truffle/hdwallet-provider')
-const uniswapPrice = require('uniswap-price')
-const CoinGecko = require('coingecko-api');
+const web3 = new Web3('https://rpc-mainnet.matic.quiknode.pro')
 const request = require('request-promise');
 
-const web3 = new Web3('https://rpc-mainnet.matic.quiknode.pro')
 const ArthController = require('../deployments/abi/ArthController.json')
-//const ArthShares = require('../deployments/abi/ArthShares.json')
-//const MahaToken = require('../deployments/abi/MahaToken.json')
-
-//const UniswapV2Pair = require('../deployments/abi/UniswapV2Pair.json')
 const StakeARTHXRMAHA = require('../manualABI/BasicStakingSpecificReward.json')
 const staggingBasicStaking = require('../manualABI/staging/abi/BasicStaking.json')
 const UniswapV2Pair = require('../manualABI/staging/abi/UniswapV2Pair.json')
@@ -18,9 +10,10 @@ const UniswapV2Pair = require('../manualABI/staging/abi/UniswapV2Pair.json')
 const arthcontroller = new web3.eth.Contract(ArthController, '0x8fce074b01040805aEeBCa13a9b2500316321A18')
 const arthxmahaStakePool = new web3.eth.Contract(StakeARTHXRMAHA, '0xAd7CD904568E5a23fdbE57d6C32fB5D1f91eBd0d')
 const stakeArthxArth = new web3.eth.Contract(staggingBasicStaking, '0x83bF88AF74743916db6e140768c9F9f681A9B276')
-const ArthArthxLP = new web3.eth.Contract(UniswapV2Pair, '0x90edfEe14635de0574cb2cc210B5196B973aB1ab')
-const StakeARTH = new web3.eth.Contract(staggingBasicStaking, '0x653887B8A074DAb771Dd116473c046e7b210c68c')
-
+const stakeARTH = new web3.eth.Contract(staggingBasicStaking, '0x653887B8A074DAb771Dd116473c046e7b210c68c')
+const stakeARTHMaha = new web3.eth.Contract(staggingBasicStaking, '0xAd7CD904568E5a23fdbE57d6C32fB5D1f91eBd0d')
+const stakeARTHX = new web3.eth.Contract(staggingBasicStaking, '0x9053126c1D10F9c84Ef6F3b66152fB692a3a6c2B')
+//const ArthArthxLP = new web3.eth.Contract(UniswapV2Pair, '0x90edfEe14635de0574cb2cc210B5196B973aB1ab')
 
 const sendRequest = async (method, url, body) => {
     const option = {
@@ -68,13 +61,6 @@ const getArthxPrice = async () => {
     }
 }
 //getArthxPrice()
-
-
-const getEthGmuPrice = async () => {
-    const ethGmuPrice = await arthcontroller.methods.getETHGMUPrice().call()
-    return ethGmuPrice
-}
-
 
 const getMahaPrice = async () => {
     try {
@@ -176,8 +162,8 @@ const arthAPY = async (req, res) => {
         const mahaprice = JSON.parse(await getMahaPrice())
         const arthxPrice = await getArthxPrice()
         const arthPrice = await getArthPrice()
-        const rewardForDuration = Number(await StakeARTH.methods.getRewardForDuration().call())
-        const totalSupply = await StakeARTH.methods.totalSupply().call()
+        const rewardForDuration = Number(await stakeARTH.methods.getRewardForDuration().call())
+        const totalSupply = await stakeARTH.methods.totalSupply().call()
 
         let rewardTokenPrice = arthxPrice / mahaprice.mahadao.usd
         console.log('rewardTokenPrice', rewardTokenPrice);
@@ -195,3 +181,52 @@ const arthAPY = async (req, res) => {
 }
 //arthAPY()
 
+//StakeARTHMAHA staking contract
+const arthmaha = async () => {
+    try {
+        const mahaprice = JSON.parse(await getMahaPrice())
+        const arthxPrice = await getArthxPrice()
+        let rewardTokenPrice = arthxPrice / mahaprice.mahadao.usd
+
+        console.log('rewardTokenPrice', rewardTokenPrice);
+
+        const rewardForDuration = Number(await stakeArthxArth.methods.getRewardForDuration().call())
+        const totalSupply = await stakeArthxArth.methods.totalSupply().call()
+        console.log('totalSupply', typeof (totalSupply));
+
+        let rewardUSD = rewardTokenPrice * rewardForDuration / 1e18
+        let totalSupplyUSD = (totalSupply / 1e18) * rewardTokenPrice
+
+        let APY = ((rewardUSD / totalSupplyUSD) * 100) * 4
+        console.log('rewardUSD', rewardUSD, 'totalSupplyUSD', totalSupplyUSD, 'APY', APY);
+
+        //res.send({ APY: APY })
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+//StakeARTHX staking contract
+const basicStakingArthx = async () => {
+    try {
+        const mahaprice = JSON.parse(await getMahaPrice())
+        const arthxPrice = await getArthxPrice()
+        let rewardTokenPrice = arthxPrice / mahaprice.mahadao.usd
+
+        console.log('rewardTokenPrice', rewardTokenPrice);
+
+        const rewardForDuration = Number(await stakeARTHX.methods.getRewardForDuration().call())
+        const totalSupply = await stakeARTHX.methods.totalSupply().call()
+        console.log('totalSupply', typeof (totalSupply));
+
+        let rewardUSD = rewardTokenPrice * rewardForDuration / 1e18
+        let totalSupplyUSD = (totalSupply / 1e18) * rewardTokenPrice
+
+        let APY = ((rewardUSD / totalSupplyUSD) * 100) * 4
+        console.log('rewardUSD', rewardUSD, 'totalSupplyUSD', totalSupplyUSD, 'APY', APY);
+
+        //res.send({ APY: APY })
+    } catch (e) {
+        console.log(e);
+    }
+}
