4ed9349436900c3748c2dd72a007a623abb34674
Sagar Behara <sagar@cryptocontrol.io>
2021-07-03 13:32:57 +0530

    added an entry file to server from production

--- index.js
+++ index.js
@@ -1,25 +1,21 @@
-import routes from './routes'
-import apicache from 'apicache'
-import { cronJob } from './controller/cron'
+/**
+ * Entry Script
+ */
+require('dotenv').config()
 
-const express = require('express')
-let cache = apicache.middleware
-const app = express()
-const bodyParser = require('body-parser')
-const cors = require('cors')
 
-app.use(cors())
-app.use(bodyParser.json({ limit: '100mb' }))
-app.use(bodyParser.urlencoded({ extended: true, limit: '100mb' }))
-app.use(cache('60 minutes'))
-app.use(routes)
+process.env.ROOT_PATH = __dirname
 
-cronJob()
+// avoid loading asset files on the server-side
+require.extensions['.png'] = function () { }
+require.extensions['.sass'] = function () { }
+require.extensions['.css'] = function () { }
+require.extensions['.gif'] = function () { }
 
-app.get('/', function (req, res) {
-    res.send('Hello World')
-})
 
-app.listen(3000, () => {
-    console.log('App running on 3000');
-})
+if (process.env.NODE_ENV == 'production' || process.env.NODE_ENV == 'test') {
+  require('./dist/')
+  return
+}
+
+require('./server')
