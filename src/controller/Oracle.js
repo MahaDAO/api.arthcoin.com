809242befa5cdf722f331a144b461eba41a711f6
Steven Enamakel <enamakel@vezures.com>
2021-07-05 01:44:57 +0300

    added the oracle updater

--- src/controller/Oracle.js
+++ src/controller/Oracle.js
@@ -7,15 +7,14 @@ const from = process.env.FROM_ADDRESS;
 const privateKey = process.env.WALLET_ADDRESS;
 const rpc = process.env.RPC_URL;
 
-const UniswapPairOracle = require('./deployments/abi/UniswapPairOracle.json');
+const ABI = require('./deployments/abi/OracleUpdater.json');
 
 export const updateOracles = async () => {
     const provider = new Provider(privateKey, rpc);
     const web3 = new Web3(provider);
 
-    const UniswapPairOracle_ARTH_ARTHX = new web3.eth.Contract(UniswapPairOracle, '0x23b8603CB7d8395754E20e272e0e2fFFA2828654');
-    const UniswapPairOracle_ARTH_MAHA = new web3.eth.Contract(UniswapPairOracle, '0xFC645E3c39e257d634bBea9637a4c7f326eB4B50');
-    const UniswapPairOracle_ARTH_USDC = new web3.eth.Contract(UniswapPairOracle, '0x7F4f57b81c1134eFC6c0FB7B51FDF50eEb6afdbc');
+
+    const instance = new web3.eth.Contract(ABI, '0x8E98466623E5Af52a7c06045eB1ebE074A2d1eD6');
 
     const getSendParams = async (nonceBump = 0) => {
         return {
@@ -26,18 +25,9 @@ export const updateOracles = async () => {
     };
 
     try {
-
-        console.log('updating UniswapPairOracle_ARTH_MAHA oracle');
-        const receipt1 = await UniswapPairOracle_ARTH_MAHA.methods.update().send(await getSendParams());
-        console.log('UniswapPairOracle_ARTH_MAHA updated; tx hash', receipt1.transactionHash);
-
-        console.log('updating UniswapPairOracle_ARTH_ARTHX oracle');
-        const receipt2 = await UniswapPairOracle_ARTH_ARTHX.methods.update().send(await getSendParams());
-        console.log('UniswapPairOracle_ARTH_ARTHX updated; tx hash', receipt2.transactionHash);
-
-        console.log('updating UniswapPairOracle_ARTH_USDC oracle');
-        const receipt3 = await UniswapPairOracle_ARTH_USDC.methods.update().send(await getSendParams());
-        console.log('UniswapPairOracle_ARTH_USDC updated; tx hash', receipt3.transactionHash);
+        console.log('updating oracles');
+        const receipt1 = await instance.methods.update().send(await getSendParams());
+        console.log('oracles updated; tx hash', receipt1.transactionHash);
 
     } catch (e) {
         console.log('BondRedemtionOracle tx filed; nvm', e);
--- src/controller/cron/index.js
+++ src/controller/cron/index.js
@@ -3,9 +3,9 @@ import { updateOracles } from "../Oracle"
 const cron = require('node-cron');
 
 export const cronJob = async () => {
-    cron.schedule('0 * * * *', () => {
-        //updateOracles()
-        console.log('running a task every hour');
+    cron.schedule('*/10 * * * *', () => {
+        console.log('running a task every 10 mins');
+        updateOracles();
     });
 };
 
