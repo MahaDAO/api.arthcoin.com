ec028f8c4b98dab44f71a0180f6c863e24d10adc
Steven Enamakel <enamakel@vezures.com>
2022-02-12 15:45:29 +0000

    update governance APRs

--- src/controller/governance.ts
+++ src/controller/governance.ts
@@ -7,8 +7,7 @@ import { polygonProvider } from "../web3";
 const cache = new NodeCache();
 
 // ABIs
-const BasicStakingABI = require("../abi/BasicStaking.json");
-const IERC20ABI = require("../abi/IERC20.json");
+const VotingEscrow = require("../abi/VotingEscrow.json");
 
 const tokenAddresses = {
   arth: "0xe52509181feb30eb4979e29ec70d50fd5c44d590",
@@ -16,132 +15,72 @@ const tokenAddresses = {
   usdc: "0x2791bca1f2de4661ed88a30c99a7a9449aa84174",
   sclp: "0x2fc711518aae7c87d7002566c5d43b0e5d2b1932",
   poolToken: "0x48202b3f81e345c8F72aae88cC386dD1fbBeab97",
+  escrow: "0x8f2c37d2f8ae7bce07aa79c768cc03ab0e5ae9ae",
 };
 
-const stakingAddresses = {
-  stakeMahax: "0x89DA855d94dD60396E585B19072A30397A9355A1",
-};
+const bigE18 = BigNumber.from(10).pow(18);
 
-const tokenDecimals = {
-  USDC: 6,
-  ARTH: 18,
-  MAHA: 18,
-  SCLP: 18,
-};
+const mahaRewardPerYear = 60000;
+const sclpRewardPerYear = 1000000;
+const forwardRewardPerYear = 5000000000;
+const usdcRewardPerYear = 0; // todo remove this hardcoded values
+const arthRewardPerYear = 0;
 
-const maha = new ethers.Contract(
-  tokenAddresses.maha,
-  IERC20ABI,
-  polygonProvider
-);
-const usdc = new ethers.Contract(
-  tokenAddresses.usdc,
-  IERC20ABI,
-  polygonProvider
-);
-const sclp = new ethers.Contract(
-  tokenAddresses.sclp,
-  IERC20ABI,
-  polygonProvider
-);
-const arth = new ethers.Contract(
-  tokenAddresses.arth,
-  IERC20ABI,
-  polygonProvider
-);
-
-const _getAPYforTokensStakingContract = async (
-  collateralPrices: ICollateralPrices,
-  tokenAddress: string,
-  contract: string,
-  quarters: number
-) => {
-  const token = new ethers.Contract(tokenAddress, IERC20ABI, polygonProvider);
-
-  const stakingContract = new ethers.Contract(
-    contract,
-    BasicStakingABI,
-    polygonProvider
-  );
-
-  let collateral;
-  let rewardsTokenRemaining;
-
-  if (token === usdc) {
-    collateral = "USDC";
-    rewardsTokenRemaining =
-      (await usdc.balanceOf(tokenAddresses.poolToken)) / 10 ** 18;
-  } else if (token === maha) {
-    collateral = "MAHA";
-    rewardsTokenRemaining =
-      (await maha.balanceOf(tokenAddresses.poolToken)) / 10 ** 18;
-    // } else if (token === sclp){
-    //     collateral = 'SCLP'
-    //     rewardsTokenRemaining = (await sclp.balanceOf(tokenAddresses.poolToken)) / 10 ** 18
-  } else if (token === arth) {
-    collateral = "ARTH";
-    rewardsTokenRemaining =
-      (await arth.balanceOf(tokenAddresses.poolToken)) / 10 ** 18;
-  }
-
-  const priceOfToken = collateralPrices[collateral];
-  const priceOfOnePoolToken = 0;
-  //  await _getUSDValueOfOnePoolToken(
-  //   collateralPrices
-  // );
-  // const rewardTokensRemaining = (await token.balanceOf(contract)) / 10 ** 18
-  let totalLockedTokenValue =
-    Number(await stakingContract.totalSupply()) / 10 ** 18;
-
-  // if (totalLockedTokenValue === 0) {
-  //   totalLockedTokenValue = 10000
-  // }
-
-  const rewardUSD = priceOfOnePoolToken * rewardsTokenRemaining;
-  const totalUSDValueLocked = priceOfToken * totalLockedTokenValue;
-
-  return (rewardUSD / totalUSDValueLocked) * 100 * quarters;
-};
-
-export const rewardAPY = async (collateralPrices: ICollateralPrices) => {
-  let arthApy = await _getAPYforTokensStakingContract(
-    collateralPrices,
-    tokenAddresses.arth,
-    stakingAddresses.stakeMahax,
-    4
-  );
-  let usdcApy = await _getAPYforTokensStakingContract(
-    collateralPrices,
-    tokenAddresses.usdc,
-    stakingAddresses.stakeMahax,
-    4
-  );
-  let mahaApy = await _getAPYforTokensStakingContract(
-    collateralPrices,
-    tokenAddresses.maha,
-    stakingAddresses.stakeMahax,
-    4
-  );
-  let sclpApy = await _getAPYforTokensStakingContract(
-    collateralPrices,
-    tokenAddresses.sclp,
-    stakingAddresses.stakeMahax,
-    4
-  );
+const getUSDRewardPerYear = async (collateralPrices: ICollateralPrices) => {
+  const mahaPrice = collateralPrices.MAHA;
+  const sclpPrice = collateralPrices.SCLP;
+  const usdcPrice = collateralPrices.USDC;
+  const arthPrice = collateralPrices.ARTH;
+  const forwardPrice = 0; // collateralPrices.Forward
 
   return {
-    arthApy: 0,
-    usdcApy: 0,
-    mahaApy: mahaApy,
-    sclpApy: sclpApy,
+    mahaUSDPerYear: mahaRewardPerYear * mahaPrice,
+    sclpUSDPerYear: sclpRewardPerYear * sclpPrice,
+    forwardUSDPerYear: forwardRewardPerYear * forwardPrice,
+    usdcUSDPerYear: usdcRewardPerYear * usdcPrice,
+    arthUSDPerYear: arthRewardPerYear * arthPrice,
   };
 };
 
-const fetchAPRs = async () => {
-  const collateralPrices = await getCollateralPrices();
+const getMAHARewardPerYear = async (collateralPrices: ICollateralPrices) => {
+  const tokenRewardPerYear = await getUSDRewardPerYear(collateralPrices);
+
   return {
-    // mahaxApy: await mahaxBasicQ3(collateralPrices),
-    rewardsApy: await rewardAPY(collateralPrices),
+    maha: Math.ceil(tokenRewardPerYear.mahaUSDPerYear / collateralPrices.MAHA),
+    sclp: Math.ceil(tokenRewardPerYear.sclpUSDPerYear / collateralPrices.MAHA),
+    forward: Math.ceil(tokenRewardPerYear.forwardUSDPerYear / 0), //collateralPrices.FORWARD
+    usdc: Math.ceil(tokenRewardPerYear.usdcUSDPerYear / collateralPrices.MAHA),
+    arth: Math.ceil(tokenRewardPerYear.arthUSDPerYear / collateralPrices.MAHA),
+  };
+};
+
+const getAPR = async () => {
+  const collateralPrices = await getCollateralPrices();
+  const rewardWorthMahaPerYear = await getMAHARewardPerYear(collateralPrices);
+
+  const mahax = new ethers.Contract(
+    tokenAddresses.escrow,
+    VotingEscrow,
+    polygonProvider
+  );
+
+  const totalLockedMaha = (await mahax.totalSupplyWithoutDecay())
+    .div(bigE18)
+    .toNumber();
+
+  const mahaAPY = (rewardWorthMahaPerYear.maha / totalLockedMaha) * 100;
+  const sclpAPY = (rewardWorthMahaPerYear.sclp / totalLockedMaha) * 100;
+  const forwardAPY = (rewardWorthMahaPerYear.forward / totalLockedMaha) * 100;
+  const usdcAPY = (rewardWorthMahaPerYear.usdc / totalLockedMaha) * 100;
+  const arthAPY = (rewardWorthMahaPerYear.arth / totalLockedMaha) * 100;
+
+  return {
+    mahaAPY,
+    sclpAPY,
+    forwardAPY,
+    usdcAPY: usdcAPY,
+    arthAPY: arthAPY,
+    overallAPY: mahaAPY + sclpAPY + 0 + usdcAPY + arthAPY,
   };
 };
 
@@ -153,7 +92,7 @@ export default async (_req, res) => {
   if (cache.get("governance-apr")) {
     res.send(cache.get("governance-apr"));
   } else {
-    const data = await fetchAPRs();
+    const data = await getAPR();
     cache.set("governance-apr", JSON.stringify(data), 60);
     res.send(JSON.stringify(data));
   }
