86feede3cd0bf47454f704932f058491273331cf
Steven Enamakel <enamakel@vezures.com>
2022-03-13 14:54:00 +0000

    added lp cal for 3pools

--- src/controller/coingecko.ts
+++ src/controller/coingecko.ts
@@ -7,6 +7,10 @@ export type CollateralKeys =
   | "WBTC"
   | "BUSD"
   | "USDT"
+  | "ARTH.usd"
+  | "bsc.3eps"
+  | "polygon.3pool"
+  | "DAI"
   | "WETH"
   | "MAHA"
   | "ARTH"
@@ -18,7 +22,7 @@ export type ICollateralPrices = {
 
 export const getCollateralPrices = async (): Promise<ICollateralPrices> => {
   const result = await CoinGeckoClient.simple.price({
-    ids: "bitcoin,ethereum,tether,mahadao,arth,usd-coin,scallop,binance-usd",
+    ids: "bitcoin,ethereum,dai,tether,mahadao,arth,usd-coin,scallop,binance-usd",
     vs_currencies: "USD",
   });
 
@@ -27,6 +31,10 @@ export const getCollateralPrices = async (): Promise<ICollateralPrices> => {
     WBTC: result.data.bitcoin.usd,
     BUSD: result.data["binance-usd"].usd,
     USDT: result.data.tether.usd,
+    "polygon.3pool": 1.048,
+    "ARTH.usd": 1,
+    "bsc.3eps": 1.0392,
+    DAI: result.data.dai.usd,
     USDC: result.data["usd-coin"].usd,
     WETH: result.data.ethereum.usd,
     MAHA: result.data.mahadao.usd,
