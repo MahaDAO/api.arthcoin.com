09a3f34541187febf712746754bd65b8231f050c
Sagar133 <sagar@vezures.com>
2022-06-24 14:37:32 +0530

    apr improvement

--- src/controller/scrapedApy/apr.ts
+++ src/controller/scrapedApy/apr.ts
@@ -14,7 +14,7 @@ export const scrappedApr = async () => {
     
     const rawDataDot = await request('https://api.dotdot.finance/api/lpDetails')
     const dotDataJson = JSON.parse(rawDataDot)
-    console.log('dotDataJson', dotDataJson, dotDataJson.data);
+    //console.log('dotDataJson', dotDataJson, dotDataJson.data);
     
     const dotData = dotDataJson.data.tokens[18]
     const dotDddApr = dotData.realDddAPR
@@ -23,12 +23,30 @@ export const scrappedApr = async () => {
     const dotTvl = dotData.dddTvlUSD + dotData.epsTvlUSD
     //console.log(dotData, dotTvl);
     
-    const dotDataArthu3eps = dotDataJson.data.tokens[18]
-    const dotDddAprArthu3eps = dotData.realDddAPR
-    const dotEpxAprArthu3eps = dotData.realEpxAPR
-    const dotApyArthu3eps = dotData.realDddAPR + dotData.realEpxAPR
-    const dotTvlArthu3eps = dotData.dddTvlUSD + dotData.epsTvlUSD
+    const dotDataArthu3eps = dotDataJson.data.tokens[25]
+    const dotDddAprArthu3eps = dotDataArthu3eps.realDddAPR
+    const dotEpxAprArthu3eps = dotDataArthu3eps.realEpxAPR
+    const dotApyArthu3eps = dotDataArthu3eps.realDddAPR + dotDataArthu3eps.realEpxAPR
+    const dotTvlArthu3eps = dotDataArthu3eps.dddTvlUSD //+ dotDataArthu3eps.epsTvlUSD
 
+    console.log({
+        ellipsis: epxAprWithBoost,
+        ellipsisMinApr: epxAprWithoutBoost,
+        ellipsisMaxApr: epxAprWithBoost,
+
+        dotDddApr: dotDddApr,
+        dotEpxApr: dotEpxApr,
+        dot: dotApy,
+
+        dotDddAprArthu3eps: dotDddAprArthu3eps,
+        dotEpxAprArthu3eps: dotEpxAprArthu3eps,
+        dotApyArthu3eps: dotApyArthu3eps,
+
+        ellipsisTvl: epxTvl,
+        dotTvl: dotTvl,
+        dotTvlArthu3eps: dotTvlArthu3eps
+    })
+    
     return {
         ellipsis: epxAprWithBoost,
         ellipsisMinApr: epxAprWithoutBoost,
