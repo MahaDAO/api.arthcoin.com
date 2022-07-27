2f3d7763bc898c7f60282f49a5804c0ed4a8064e
Sagar133 <sagar@vezures.com>
2022-07-27 13:26:10 +0530

    made the rewards tvl dynamic

--- src/controller/stabiltyPoolApy.ts
+++ src/controller/stabiltyPoolApy.ts
@@ -43,22 +43,41 @@ const wallet = new ethers.Wallet(
 )
 
 export const getTokenAddress = async (chainid) => {
-    let tokenAddress 
-    switch (chainid) {
-        case (137):
-            tokenAddress = "0x862Fc9F243365d98Ed9FF68f720041074299B0dC";
-        break;
-        case (56):
-            tokenAddress = "0x85daB10c3BA20148cA60C2eb955e1F8ffE9eAa79";
-        break;
-        case (1):
-            tokenAddress = "0x8CC0F052fff7eaD7f2EdCCcaC895502E884a8a71";
-        break;
-        default:
-            tokenAddress = "NEW";
-    }
+  let tokenAddress 
+  switch (chainid) {
+      case (137):
+          tokenAddress = "0x862Fc9F243365d98Ed9FF68f720041074299B0dC";
+      break;
+      case (56):
+          tokenAddress = "0x85daB10c3BA20148cA60C2eb955e1F8ffE9eAa79";
+      break;
+      case (1):
+          tokenAddress = "0x8CC0F052fff7eaD7f2EdCCcaC895502E884a8a71";
+      break;
+      default:
+          tokenAddress = "NEW";
+  }
 
-    return tokenAddress
+  return tokenAddress
+}
+
+export const getMahaAddress = async (chainid) => {
+  let tokenAddress 
+  switch (chainid) {
+      case (137):
+          tokenAddress = "0xeDd6cA8A4202d4a36611e2fff109648c4863ae19";
+      break;
+      case (56):
+          tokenAddress = "0xCE86F7fcD3B40791F63B86C3ea3B8B355Ce2685b";
+      break;
+      case (1):
+          tokenAddress = "0xB4d930279552397bbA2ee473229f89Ec245bc365";
+      break;
+      default:
+          tokenAddress = "NEW";
+  }
+
+  return tokenAddress
 }
 
 
@@ -68,11 +87,28 @@ const getAPR = async (
   monthlyRewardinMAHA
 ) => { 
     const rewardinUSD = 12 * monthlyRewardinMAHA * collateralPrices.MAHA;  
-
-    console.log("APR", (rewardinUSD / contractTVLinUSD) * 100);
-    
     return (rewardinUSD / contractTVLinUSD) * 100;
 };
+
+const getRewardBalance = async (
+  stabilityPool,
+    chainid,
+    provider
+) => {
+  const tokenAddress = await getMahaAddress(chainid)
+  console.log("Maha", tokenAddress, chainid, stabilityPool);
+  
+  const maha = new ethers.Contract(
+      tokenAddress,
+      IERC20,
+      provider
+  );
+
+  const balance = await maha.balanceOf(stabilityPool)
+  console.log("balance", Number(balance / 1e18));
+  
+  return Number(balance / 1e18)
+}
 
 const getTVL = async (
     stabilityPool,
@@ -87,10 +123,6 @@ const getTVL = async (
     );
 
     const balance = await arth.balanceOf(stabilityPool)
-
-    console.log("TVL", balance/ 1e18);
-    
-
     return Number(balance / 1e18)
 }
 
@@ -110,19 +142,22 @@ const fetchAPRs = async () => {
     const collateralPrices = await getCollateralPrices();
 
     const polygonStabilytvl = await getTVL("0x9209757eC192caA894Ad8eBC393DeB95b2ed5d0a", 137, polygonProvider)
-    //console.log('arth balance', polygonStabilytvl); 
+    const polygonStabilyRewardtvl = await getRewardBalance("0x9209757eC192caA894Ad8eBC393DeB95b2ed5d0a", 137, polygonProvider)
+    console.log('polygonStabilyRewardtvl', polygonStabilyRewardtvl); 
     const tvlInUsdPolygon = polygonStabilytvl * collateralPrices.ARTH || 2
-    const polygonApr = await getAPR(tvlInUsdPolygon, collateralPrices, 0)
+    const polygonApr = await getAPR(tvlInUsdPolygon, collateralPrices,   0)
 
     const bnbStabilytvl = await getTVL("0x61A787B3E2eE1e410310fC7c4A9f6C77430e1B57", 56, bscProvider)
-    //console.log('arth balance', bnbStabilytvl);
+    const bnbnStabilyRewardtvl = await getRewardBalance("0x61A787B3E2eE1e410310fC7c4A9f6C77430e1B57", 56, bscProvider)
+    console.log('bnbnStabilyRewardtvl', bnbnStabilyRewardtvl);
     const tvlInUsdBnb = bnbStabilytvl * collateralPrices.ARTH || 2
-    const bnbApr = await getAPR(tvlInUsdBnb, collateralPrices, 500)
+    const bnbApr = await getAPR(tvlInUsdBnb, collateralPrices, bnbnStabilyRewardtvl || 500)
 
     const ethStabilytvl = await getTVL("0x2c360b513AE52947EEb37cfAD57ac9B7c9373e1B", 1, ethProvider)
-    //console.log('arth balance', bnbStabilytvl);
+    const ethStabilyRewardtvl = await getRewardBalance("0x2c360b513AE52947EEb37cfAD57ac9B7c9373e1B", 1, ethProvider)
+    console.log('ethStabilyRewardtvl', ethStabilyRewardtvl);
     const tvlInUsdEth = ethStabilytvl * collateralPrices.ARTH || 2
-    const ethApr = await getAPR(tvlInUsdEth, collateralPrices, 1000)
+    const ethApr = await getAPR(tvlInUsdEth, collateralPrices, ethStabilyRewardtvl || 1000)
 
     return {   
         matic: String(polygonApr),
