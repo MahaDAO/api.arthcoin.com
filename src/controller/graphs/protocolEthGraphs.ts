7a7e9a2e01810c2891e7fcb24f0caa0b1140a425
Sagar133 <sagar@vezures.com>
2022-08-18 16:25:26 +0530

    updated length of eth price

--- src/controller/graphs/protocolEthGraphs.ts
+++ src/controller/graphs/protocolEthGraphs.ts
@@ -12,10 +12,10 @@ const request = require('request-promise')
 const apiKey = process.env.ETHERSCAN_KEY
 const topic = '0x4d29de21de555af78a62fc82dd4bc05e9ae5b0660a37f04729527e0f22780cd3'
 
-const ethPrice = async () => {
+const ethPrice = async (length) => {
     let priceChart = await CoinGeckoClient.coins.fetchMarketChart('ethereum', {
         vs_currency: "USD",
-        days: '30',
+        days: length,
         interval: 'daily'
     });
 
@@ -37,10 +37,10 @@ const main = async (address) => {
         ])
     })
 
-    console.log(dataArray.reverse()[0].time, dataArray.reverse()[20].time, dataArray.length);
+    //console.log(dataArray.reverse()[0].time, dataArray.reverse()[20].time, dataArray.length);
     
     //console.log(dataArray.reverse());
-    let price = await ethPrice()
+    let price = await ethPrice(dataArray.length)
     // const ethPriceArray = []
     // const ethDatapoints = await price.forEach((val, i) => {        
     //     ethPriceArray.push(
