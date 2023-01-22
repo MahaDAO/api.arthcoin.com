997c06f571b1dad7fe41583d7365aeb3f7fa9ba8
Steven Enamakel <enamakel@mahadao.com>
2023-01-22 20:38:33 +0530

    disable mongodb for now

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,8 @@ import express from "express";
 import logger from "morgan";
 
 import routes from "./routes";
-import { open } from "./database/index";
+// import { open } from "./database/index";
+// open();
 
 const app = express();
 const cors = require("cors");
@@ -16,7 +17,5 @@ app.use(bodyParser.urlencoded({ extended: true, limit: "100mb" }));
 app.use(cors());
 app.use(logger("dev", { skip: () => app.get("env") === "test" }));
 app.use(routes);
-
-open();
 
 export default app;
